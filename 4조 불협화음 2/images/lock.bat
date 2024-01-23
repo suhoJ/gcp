f6736bc9123e520f567b00e736bc7b573ac7b013
suhoJ <95326370+suhoJ@users.noreply.github.com>
2024-01-23 15:22:02 +0900

    Create lock.bat

---
+++ 4조 불협화음 2/images/lock.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+if EXIST "Locker" goto UNLOCK
+if NOT EXIST Locker mkdir Locker
+echo Enter password to lock folder
+set/p "pass=>"
+if %pass%==YOUR_PASSWORD goto LOCK
+goto END
+
+:UNLOCK
+echo Enter password to unlock folder
+set/p "pass=>"
+if %pass%==gcp goto START
+goto FAIL
+
+:LOCK
+ren Locker "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+echo Folder locked
+goto End
+
+:START
+attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Locker
+echo Folder Unlocked successfully
+goto End
+
+:FAIL
+echo Invalid password
+goto end
+
+:End
